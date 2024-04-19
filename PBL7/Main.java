f38dbbbeb5ccd621051e4c3f7b97ec67c306be60
WordyDiamond <165914464+WordyDiamond@users.noreply.github.com>
2024-04-19 09:23:54 -0300

    Update Main.java

--- PBL7/Main.java
+++ PBL7/Main.java
@@ -3,6 +3,4 @@ package PBL7;
 public class Main {
     public static void main(String[] args)
     { Ebook ebook= new Ebook("Senhor dos anéis", "J.R.R Tolkien");}
-
-    Ebook ebook= new Ebook("Senhor dos anéis", "J.R.R Tolkien");
 }
